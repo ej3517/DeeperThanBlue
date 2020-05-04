47fb527bcfdf1e7cdf5e5de4d0740a8cf6869b32
Toby Hillier <44164605+tobhil98@users.noreply.github.com>
2020-05-04 15:51:27 +0200

    Add: Water animation

---
+++ UnityGame/Assets/CustomScripts/IsometricPlayerMovementController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricPlayerMovementController : MonoBehaviour
+{
+
+    public float movementSpeed = 1f;
+    //IsometricCharacterRenderer isoRenderer;
+
+    Rigidbody2D rbody;
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        //isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 currentPos = rbody.position;
+        float currentAng = rbody.rotation;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * movementSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        float newAng = Vector2.Angle(newPos, currentPos);
+        //isoRenderer.SetDirection(movement);
+        //rbody.MoveRotation(currentAng+1);
+        rbody.MovePosition(newPos);
+    }
+}
