74a0942847b21e599e1d0b920ccffccd660a3601
ej3517 <ej3517@ic.ac.uk>
2020-05-07 20:01:47 +0200

    test

---
+++ FlappyFish/Assets/Scripts/RestartButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartButton : MonoBehaviour
+{
+    private void restartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+}
