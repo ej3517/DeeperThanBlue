96ff0867eddb3cba2a35b0d646fb67c0486ece25
ej3517 <ej3517@ic.ac.uk>
2020-05-08 11:02:05 +0200

    MainMenu/ButtonHandler

---
+++ FlappyFish/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public class RestartButton : MonoBehaviour
+    {
+        public void restartButton()
+        {
+            Loader.Load(Loader.Scene.GameScene);
+        }
+    }
+}
