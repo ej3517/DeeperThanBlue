c953f84dbad1a6d9c71d84d93b8c6e871df04d7a
ej3517 <ej3517@ic.ac.uk>
2020-05-14 17:53:03 +0200

    Boat.cs

---
+++ FlappyFish/Assets/Scripts/Boat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour
+{
+    private const float BOAT_Y_POSITION = 42f;
+    private const float BOAT_WIDTH = 5f;
+    private const float BOAT_HEIGHT = 4f;
+
+    public float boatSpeed = 5f;
+    
+    
+
+}
