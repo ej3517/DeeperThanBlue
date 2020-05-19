5fa3c6399dfddc4649bb7dd3519309589aa5639e
Toby Hillier <44164605+tobhil98@users.noreply.github.com>
2020-05-19 20:33:02 +0200

    Added very simple grid

---
+++ FlappyFish/Assets/Scripts/CodeChallenge/Grid.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Grid
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] gridArray;
+    private Transform transform;
+
+
+    public Grid(int _width, int _height, float _cellSize, Transform _transform)
+    {
+        width = _width;
+        height = _height;
+        cellSize = _cellSize;
+        transform = _transform;
+
+        gridArray = new int[width, height];
+        
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for(int y = 0; y< gridArray.GetLength(1); y++)
+            {
+                TextMesh textMesh = UtilsClass.CreateWorldText(gridArray[x,y].ToString(), transform, GetWorldPosition(x,y)+ new Vector3(cellSize,cellSize)/2, 40, Color.black, TextAnchor.MiddleCenter);
+                textMesh.characterSize = 3;
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.black, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.black, 100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.black, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.black, 100f);
+
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize;
+    }
+
+    
+}
--- FlappyFish/Assets/Scripts/CodeChallenge/Testing.cs
+++ FlappyFish/Assets/Scripts/CodeChallenge/Testing.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CodeChallenge : MonoBehaviour
+public class Testing : MonoBehaviour
 {
     // Start is called before the first frame update
-
-
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        Grid grid = new Grid(8, 10, 50f, transform);
     }
+
 }
