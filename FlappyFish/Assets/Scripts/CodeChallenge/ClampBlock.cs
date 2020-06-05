eb2b08a80b438adc94fbd7a8ddf0d233507edb22
Toby Hillier <44164605+tobhil98@users.noreply.github.com>
2020-06-05 23:00:39 +0100

    Blocks area first version

---
+++ FlappyFish/Assets/Scripts/CodeChallenge/ClampBlock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClampBlock : MonoBehaviour
+{
+    ScrollRect scroll;
+    
+    private void Awake()
+    {
+        scroll = GetComponent<ScrollRect>();
+    }
+
+
+    private void Update()
+    {
+        //Debug.Log(transform.position.x);
+        if(transform.position.x>250)
+        {
+            scroll.inertia = false;
+            scroll.StopMovement();
+            transform.position = new Vector3(250, transform.position.y, transform.position.z);
+            scroll.inertia = true;
+        }
+        else if (transform.position.x < -250)
+        {
+            scroll.inertia = false;
+            scroll.StopMovement();
+            transform.position = new Vector3(-250, transform.position.y, transform.position.z);
+            scroll.inertia = true;
+        }
+    }
+}
