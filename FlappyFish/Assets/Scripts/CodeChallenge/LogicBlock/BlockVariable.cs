985c7ffa5ea018c304b65a63f782db64ea806276
Toby Hillier <44164605+tobhil98@users.noreply.github.com>
2020-06-13 14:56:31 +0100

    Created Variable block

---
+++ FlappyFish/Assets/Scripts/CodeChallenge/LogicBlock/BlockVariable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockVariable : Block
+{
+    private void Awake()
+    {
+        SetType("Variable");
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public override IEnumerator Traverse(Transform Button)
+    {
+        //Sett button as child
+        Button.SetParent(transform);        // Maybe not needed
+        Button.localPosition = new Vector3(-200, 6);
+
+        Button.GetComponent<StartButton>().Forward();
+
+        yield return new WaitForSeconds(Globals.CodeChallengeSpeed);
+        if (belowBlock != null)
+        {
+            StartCoroutine(belowBlock.Traverse(Button));
+        }
+        else
+        {
+            Button.GetComponent<StartButton>().Restart();
+        }
+    }
+
+}
