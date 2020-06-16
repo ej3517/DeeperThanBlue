9631ddab8b28d790b02894ec15d2f0fd6566019c
Joao Baptista <joao.baptista17@imperial.ac.uk>
2020-06-16 13:17:02 +0100

    added back to menu button on leaderboard

---
+++ FlappyFish/Assets/Scripts/toMainMenuButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toMainMenuButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toMainMenuScene(){
+        // SoundManager.PlaySound(SoundManager.Sound.ButtonClick);
+        Loader.Load(Loader.Scene.MainMenu);
+    }
+}
