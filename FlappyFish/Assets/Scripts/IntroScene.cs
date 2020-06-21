0bf540eaa238e356be2cc81252ce7268c067377e
Toby Hillier <44164605+tobhil98@users.noreply.github.com>
2020-06-21 14:33:41 +0100

    Added "In partnership with"

---
+++ FlappyFish/Assets/Scripts/IntroScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CallScene());
+    }
+
+    // Update is called once per frame
+    IEnumerator CallScene()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(Loader.Scene.LoginScene.ToString());
+    }
+}
